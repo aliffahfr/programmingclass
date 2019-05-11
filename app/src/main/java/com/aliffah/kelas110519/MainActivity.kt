a5bb1bbdb1409adc8feb26f9932a076eb43d52ec
980705 <aliffahfr@gmail.com>
2019-05-11 14:22:48 +0700

    first commit

---
+++ app/src/main/java/com/aliffah/kelas110519/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.aliffah.kelas110519
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.add_friend.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/aliffah/kelas110519/teman.kt
@@ -0,0 +1,31 @@
+package com.aliffah.kelas110519
+
+import android.graphics.drawable.DrawableContainer
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.*
+
+class Teman : Fragment(){
+    companion object {
+        fun newInstance(): Teman {
+            return Teman()
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.add_friend, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        this.clearFindViewByIdCache()
+    }
+}
